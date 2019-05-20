0801a55d0a76f0c20ff5b33d00317494a76fb3e5
mcumm64 <mcumm64@gmail.com>
2019-05-20 21:54:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def dictionary
+   {
+    "hello" => "hi",
+    "to, two, too" => '2', 
+    "for, four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@" ,
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_word_arr = tweet.split(" ")
+  
+  x = tweet_word_arr.collect do |word|
+      if dictionary.keys.include?(word)
+        dictionary.collect do |term, abbrev|
+          if word == term
+            word = abbrev
+          end 
+        end
+      else
+      end
+  end
+
+x.flatten.compact.join(" ")
+end
